256cd0c39497a56cd5eb65a969c012e499a76aec
Max Naumchyk <max.naumch@gmail.com>
2024-10-03 19:20:53 +0300

    Rethink how I take care of types

--- permute_front/src/yaml/hir.rs
+++ permute_front/src/yaml/hir.rs
@@ -1,10 +1,8 @@
 use compact_str::CompactString;
 use hashbrown::HashMap;
-use proc_macro2::Span;
 use smallvec::SmallVec;
 
 type IdentId = u16;
-type TypeId = u16;
 
 pub struct Main {
     /// Name of the project. Cannot be empty.
@@ -23,10 +21,8 @@ pub struct Main {
     /// Identifiers of bindings used in the main file. These are valid Rust identifiers.
     idents: Vec<CompactString>,
 
-    /// List of types that are imported from other modules. This is done via "use" clause.
-    /// This unwraps all "use"s when they are both just paths like "std::slice::Iter" and
-    /// other more complex ones.
-    imported_ty: Vec<ImportedTy>,
+    /// Use clauses for the main file. These are used to import types from other modules.
+    uses: Vec<syn::UseTree>,
 }
 
 impl Main {
@@ -52,69 +48,8 @@ impl Main {
         })
     }
 
-    pub fn imported_tys(&self) -> impl Iterator<Item = &ImportedTy> {
-        self.imported_ty.iter()
-    }
-
-    pub fn get_imported_ty(&self, id: TypeId) -> Option<&ImportedTy> {
-        self.imported_ty.get(id as usize)
-    }
-
     pub fn get_binding(&self, id: IdentId) -> Option<&MainBinding> {
         self.bindings.get(&id)
-    }
-
-    /// Get type information for the given binding from this [Main].
-    pub fn get_binding_ty<'a>(&'a self, binding: &'a MainBinding) -> Option<&'a ImportedTy> {
-        self.get_imported_ty(binding.ty)
-    }
-}
-
-pub struct ImportedTy {
-    rename: Option<CompactString>,
-    segments: SmallVec<[CompactString; 8]>,
-}
-
-impl ImportedTy {
-    pub fn name(&self) -> &str {
-        self.rename
-            .as_ref()
-            .map(|s| s.as_str())
-            .unwrap_or_else(|| self.segments.last().unwrap().as_str())
-    }
-
-    pub fn from_segments(segments: impl Iterator<Item = String>) -> Self {
-        let segments = segments.map(CompactString::from).collect();
-        Self {
-            rename: None,
-            segments,
-        }
-    }
-
-    pub fn from_segments_with_rename(
-        segments: impl Iterator<Item = String>,
-        rename: String,
-    ) -> Self {
-        let segments = segments.map(CompactString::from).collect();
-        Self {
-            rename: Some(CompactString::from(rename)),
-            segments,
-        }
-    }
-
-    pub fn syn_path(&self) -> syn::Path {
-        let segments = self.segments.iter().map(|s| s.as_str());
-        let mut path = syn::Path {
-            leading_colon: None,
-            segments: Default::default(),
-        };
-        for segment in segments {
-            path.segments.push(syn::PathSegment {
-                ident: syn::Ident::new(segment, Span::call_site()),
-                arguments: syn::PathArguments::None,
-            });
-        }
-        path
     }
 }
 
@@ -134,55 +69,9 @@ impl<'main> Pipe<'main> {
     }
 }
 
-/// Connection between some item's type and optional related "use" clause.
-pub struct TyPath {
-    /// Optional import for the type if it was found. Otherwise, this is None and
-    /// the [Self::ty] is considered to be "crate::" relative.
-    imported_ty: Option<ImportedTy>,
-
-    /// Type as it was declared. May be relative to the current module.
-    ty: syn::TypePath,
-}
-
-impl TyPath {
-    /// Get the merged path of the type and the imported type. This should output
-    /// the full type path that can be used during codegen to refer to the type correctly.
-    pub fn syn_path(&self) -> syn::Path {
-        let mut path = self.ty.path.clone();
-        if let Some(item_use) = &self.imported_ty {
-            path.segments
-                .extend(item_use.segments.iter().map(|segment| syn::PathSegment {
-                    ident: syn::Ident::new(segment, Span::call_site()),
-                    arguments: syn::PathArguments::None,
-                }));
-        } else if !self.is_crate_absolute() {
-            // Make the path absolute to the crate root.
-            path.segments.insert(
-                0,
-                syn::PathSegment {
-                    ident: syn::Ident::new("crate", Span::call_site()),
-                    arguments: syn::PathArguments::None,
-                },
-            );
-        }
-        path
-    }
-
-    /// Whether the type is absolute to the crate root, i.e. it starts with "crate::".
-    pub fn is_crate_absolute(&self) -> bool {
-        self.ty.path.leading_colon.is_some()
-            || self
-                .ty
-                .path
-                .segments
-                .first()
-                .map_or(false, |s| s.ident == "crate")
-    }
-}
-
 pub struct MainBinding {
-    /// Name of the type that this binding is for.
-    ty: TypeId,
+    /// The type that this binding is for.
+    ty: syn::Type,
 
     /// Configuration for the binding. We can't validate it right now as it requires
     /// us to parse the other configuration files and get to know which fields
@@ -200,6 +89,7 @@ pub struct Sink {
     /// Name of the sink. This is a valid Rust identifier.
     name: CompactString,
 
+    /// Explanation for the sink. May be empty.
     explain: String,
 
     /// Parameters that are passed to the sink.
@@ -210,9 +100,7 @@ pub struct Sink {
     additional_checks: Vec<Check>,
 
     /// List of types that are imported from other modules. This is done via "use" clause.
-    /// This unwraps all "use"s when they are both just paths like "std::slice::Iter" and
-    /// other more complex ones.
-    imported_ty: Vec<ImportedTy>,
+    uses: Vec<syn::UseTree>,
 }
 
 pub struct Check {
@@ -228,8 +116,8 @@ pub struct Check {
 pub struct RustExpr(syn::Expr);
 
 pub struct SinkParam {
-    /// Type ID of the parameter. Refers to [Sink::imported_ty] type's index.
-    ty: TypeId,
+    /// Type of the parameter.
+    ty: syn::Type,
 
     /// Name of the parameter. This is a valid Rust identifier.
     name: CompactString,
@@ -262,17 +150,15 @@ pub struct Source {
     column_additional_checks: Vec<Check>,
 
     /// List of types that are imported from other modules. This is done via "use" clause.
-    /// This unwraps all "use"s when they are both just paths like "std::slice::Iter" and
-    /// other more complex ones.
-    imported_ty: Vec<ImportedTy>,
+    uses: Vec<syn::UseTree>,
 }
 
 pub struct SourceFilter {
     /// Explanation for the filter. May be empty.
     explain: String,
 
-    /// Type ID of the filter. Refers to [Source::imported_ty] type's index.
-    ty: TypeId,
+    /// Type of the filter.
+    ty: syn::Type,
 
     /// Default value for the filter. This is optional and may be None.
     default: Option<RustExpr>,
@@ -286,8 +172,8 @@ pub struct SourceColumn {
     /// Explanation for the column. May be empty.
     explain: String,
 
-    /// Type ID of the column. Refers to [Source::imported_ty] type's index.
-    ty: TypeId,
+    /// Type of the column.
+    ty: syn::Type,
 
     /// Checks that are performed on the column defined in the configuration.
     /// Can be none.
