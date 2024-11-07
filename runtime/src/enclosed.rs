eb3b153d7363da428d9ca421b2f1c8a526165b19
Max Naumchyk <max.naumch@gmail.com>
2024-11-07 15:42:23 +0200

    Rethinking the purpose of my life

--- runtime/src/enclosed.rs
+++
@@ -1,6 +0,0 @@
-pub fn test(s: &str) {
-    let i = 0..;
-    for _ in i {
-        lazy_regex::regex_is_match!("foo", s);
-    }
-}
--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -1,8 +1,1 @@
-#![feature(no_core)]
-
-#![no_std]
-#![no_core]
-
-extern crate permute_libcore as core;
-
 pub mod enclosed;
